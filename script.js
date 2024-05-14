8558309bc7c0a885f80cdef78b2925fbc52f9a4d
Ankitakamthe153 <88307915+Ankitakamthe153@users.noreply.github.com>
2024-05-14 14:30:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function fibonacci(num) {
 // your code here
 	if(num <= 1) return num;
-	return fibonacci(n-1) + fibonacci(n-2);
+	return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
 module.exports = fibonacci;
